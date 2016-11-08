510ea19f7596943039e33aa11efbf08456a5f26f
nlcncn <nlcncn@163.com>
2016-11-08 16:47:37 +0800

    3

---
+++ IndexBO.java
@@ -0,0 +1,11 @@
+package service;
+
+public interface IndexBO {
+	public String getHelloStr();
+
+	public void setHelloStr(String helloStr);
+
+	public String hello(String str);
+
+	public String test();
+}
